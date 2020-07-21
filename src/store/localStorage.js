993cc7b1f1f2836609f27aa8b216062126078f67
felipevianna <felipevianna@unirn.edu.br>
2020-07-20 21:31:07 -0300

    Salvando lista no localStorage.

---
+++ src/store/localStorage.js
@@ -0,0 +1,20 @@
+export const loadState = () => {
+  try {
+    const localStorageState = localStorage.getItem('list');
+    if (localStorageState === null) return undefined;
+
+    return JSON.parse(localStorageState);
+  } catch (error) {
+    console.log('Não foi possível recuperar o estado.', error);
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const localStorageState = JSON.stringify(state);
+    localStorage.setItem('list', localStorageState);
+  } catch (error) {
+    console.log('Não foi possível salvar o estado.', error);
+  }
+};
